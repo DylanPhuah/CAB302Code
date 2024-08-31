b8240531a185116f0a3dd86b9a314b19f47d2a28
z_gennerz3 <zanegenrich@gmail.com>
2024-08-31 20:51:31 +1000

    Added basic GUI layout via login-view.fxml and UniPlus.java (main), and added an empty LoginController class

---
+++ src/main/java/com/example/main/LoginController.java
@@ -0,0 +1,23 @@
+package com.example.main;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class LoginController {
+    @FXML private TextField username;
+    @FXML private TextField password;
+    @FXML private Label statusLabel;
+    @FXML private Button loginButton;
+
+    @FXML
+    protected void onLoginButtonClick() {
+
+    }
+
+    @FXML
+    protected void onRegisterButtonClick() {
+
+    }
+}
--- src/main/java/com/example/main/UniPlus.java
+++ src/main/java/com/example/main/UniPlus.java
@@ -7,17 +7,19 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class UniPlus extends Application {
+    // Initialise parameters for the login window
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(UniPlus.class.getResource("login-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 720, 480);
+        stage.setTitle("Login Screen");
         stage.setScene(scene);
         stage.show();
     }
 
     public static void main(String[] args) {
+        // Launch the login window
         launch();
     }
 }
