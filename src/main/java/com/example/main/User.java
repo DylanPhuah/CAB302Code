31e4d068d7d4cdb7da469f01d64a86504e62e0fe
z_gennerz3 <zanegenrich@gmail.com>
2024-09-13 17:57:02 +1000

    Added JavaDoc and JUnit dependencies, implemented some unit tests and comments for User class

--- src/main/java/com/example/main/User.java
+++ src/main/java/com/example/main/User.java
@@ -1,5 +1,8 @@
 package com.example.main;
 
+/**
+ * A simple object to represent a user and their data
+ */
 public class User {
     public String username;
     public String password;
@@ -7,6 +10,14 @@ public class User {
     public String lName;
     public Boolean isTeacher;
 
+    /**
+     *
+     * @param Username The username chosen by the user
+     * @param Password The password chosen by the user
+     * @param FName The first-name provided by the user
+     * @param LName The last-name provided by the user
+     * @param IsTeacher The status of the account, true for teacher or false for student
+     */
     public User(String Username, String Password, String FName, String LName, Boolean IsTeacher) {
         username = Username;
         password = Password;
