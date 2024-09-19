7923b8f1d5b294b15a0beca3e1439f56b6709c75
Juan <juan.rivaldo@connect.qut.edu.au>
2024-09-20 00:13:14 +1000

    Fixed a few issues caused after merging with main

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,7 +3,11 @@ module com.example.main {
     requires javafx.fxml;
     requires java.sql;
     requires freetts;
+    requires java.desktop;
+    requires org.apache.pdfbox;
 
+    opens com.example.PdfReader to javafx.fxml;
+    exports com.example.PdfReader to javafx.graphics;
 
     opens com.example.main to javafx.fxml;
     exports com.example.main;
